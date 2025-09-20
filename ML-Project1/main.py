92ad508ca9cc8116300032e468f9de09c1df446c
bodvarsdottirkristin <bodvarsdottirkristin@gmail.com>
2025-09-20 15:55:40 +0200

    adding prints

--- ML-Project1/main.py
+++ ML-Project1/main.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load data from csv 
 df = pd.read_csv(r'online_shoppers_intention.csv')
+print('df', df)
+
+# Split into X (features) and y (target)
+X = df.drop(columns=["Revenue"])
+y = pd.Categorical(df['Revenue']) # type: ignore[attr-defined]
+print(X)
+print(y)
+
+# Display the first few rows of the dataframe
+print(X.head())
